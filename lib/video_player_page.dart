fb460601dfe6d2e78ae0ace7c498ca4afb9e398b
junwinner <junwinner@danwi.me>
2025-04-27 10:42:26 +0800

    播放youtube视频功能实现

--- lib/video_player_page.dart
+++ lib/video_player_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-// import 'package:youtube_player_iframe/youtube_player_iframe.dart';
-
+import 'package:youtube_player_iframe/youtube_player_iframe.dart';
 class VideoPlayerPage extends StatefulWidget {
   final String youtubeUrl;
 
@@ -11,25 +10,24 @@ class VideoPlayerPage extends StatefulWidget {
 }
 
 class _VideoPlayerPageState extends State<VideoPlayerPage> {
-  // late YoutubePlayerController _controller;
-
+  late YoutubePlayerController _controller;
   @override
   void initState() {
     super.initState();
-    // _controller = YoutubePlayerController.fromVideoId(
-    //   videoId: 'jNI0fiX4q4A', // 替换为您想播放的视频ID
-    //   autoPlay: false,
-    //   params: const YoutubePlayerParams(
-    //       showControls: true,
-    //       showFullscreenButton: true,
-    //       interfaceLanguage: 'zh-CN'
-    //   ),
-    // );
+    _controller = YoutubePlayerController.fromVideoId(
+      videoId: 'jNI0fiX4q4A', // 替换为您想播放的视频ID
+      autoPlay: false,
+      params: const YoutubePlayerParams(
+          showControls: true,
+          showFullscreenButton: true,
+          interfaceLanguage: 'zh-CN'
+      ),
+    );
   }
 
   @override
   void dispose() {
-    // _controller.close();
+    _controller.close();
     super.dispose();
   }
 
@@ -37,7 +35,14 @@ class _VideoPlayerPageState extends State<VideoPlayerPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("播放YouTube视频")),
-      body: Center(child: Text('text'),),
+      body: Column(
+        children: [
+          YoutubePlayer(
+            controller: _controller,
+            aspectRatio: 16 / 9,
+          )
+        ],
+      ),
     );
   }
 }
