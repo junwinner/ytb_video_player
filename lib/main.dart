d0d8b77f6c64461a68bd67e59166b213b671febc
junwinner <junwinner@danwi.me>
2025-05-06 16:01:00 +0800

    底部模态弹窗实现

--- lib/main.dart
+++ lib/main.dart
@@ -8,27 +8,11 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
@@ -39,15 +23,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -60,32 +35,134 @@ class _MyHomePageState extends State<MyHomePage> {
   String videoUrl = 'https://www.youtube.com/watch?v=jNI0fiX4q4A';
 
   void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
+    _showBottomSheet();
+    return setState(() {
       _counter++;
     });
   }
 
+  // 选项单元格
+  Widget optionCell({
+    required String title,
+    required String subtitle,
+    TextStyle? titleStyle,
+    TextStyle? subTitleStyle,
+    required VoidCallback onTap,
+  }) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(10),
+      ),
+      padding: const EdgeInsets.symmetric(horizontal: 5),
+      child: ListTile(
+        title: Text(title, style: titleStyle ?? const TextStyle(fontSize: 18)),
+        subtitle: Padding(
+          padding: const EdgeInsets.only(top: 5),
+          child: Text(subtitle,
+              style: subTitleStyle ?? const TextStyle(fontSize: 16)),
+        ),
+        onTap: onTap,
+      ),
+    );
+  }
+
+  void _showBottomSheet() {
+    Color titleColor = const Color(0xFF323C50);
+    Color subTitleColor = const Color(0xFF8E95A3);
+    TextStyle titleStyle = TextStyle(color: titleColor, fontSize: 18);
+    TextStyle subTitleStyle = TextStyle(color: subTitleColor, fontSize: 13);
+    showModalBottomSheet(
+      context: context,
+      shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.vertical(top: Radius.circular(16)),
+      ),
+      backgroundColor: const Color(0xFFF6F8F9),
+      builder: (context) {
+        return Stack(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(
+                  left: 20, top: 10, right: 20, bottom: 60),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  // 顶部标题
+                  const Padding(
+                    padding: EdgeInsets.only(top: 15, bottom: 5),
+                    child: Center(
+                      child: Text(
+                        '导入视频或音频',
+                        style: TextStyle(
+                          fontSize: 18,
+                          color: Color(0xFF05142F),
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ),
+                  // 说明文字
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 20),
+                    child: Text(
+                      '请选择视频来源',
+                      style: TextStyle(color: subTitleColor, fontSize: 15),
+                    ),
+                  ),
+                  // 选项部分
+                  Column(
+                    children: [
+                      optionCell(
+                        title: 'Youtube 网址',
+                        subtitle: '在应用中播放Youtube视频，无需下载',
+                        titleStyle: titleStyle,
+                        subTitleStyle: subTitleStyle,
+                        onTap: () {
+                          print('点击youtube网址');
+                          Navigator.pop(context);
+                        },
+                      ),
+                      const SizedBox(height: 16),
+                      optionCell(
+                        title: '本地媒体',
+                        subtitle: '从您本地存储导入媒体文件',
+                        titleStyle: titleStyle,
+                        subTitleStyle: subTitleStyle,
+                        onTap: () => Navigator.pop(context),
+                      ),
+                      const SizedBox(height: 16),
+                      optionCell(
+                        title: '相册',
+                        subtitle: '从您的相册导入媒体文件',
+                        titleStyle: titleStyle,
+                        subTitleStyle: subTitleStyle,
+                        onTap: () => Navigator.pop(context),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            // 关闭按钮，使用 Positioned 进行绝对定位
+            Positioned(
+              top: 8,
+              right: 8,
+              child: IconButton(
+                icon: const Icon(Icons.close, size: 20),
+                onPressed: () => Navigator.pop(context),
+              ),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
         actions: [
           Padding(
@@ -105,22 +182,7 @@ class _MyHomePageState extends State<MyHomePage> {
         ],
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
